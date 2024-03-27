dbe1855b890973245d69319cb57fcbcfb0cc76cf
Daria <dariakuznetsova21@gmail.com>
2024-03-27 12:43:58 +0700

    Edited signals and slots

--- win.cpp
+++ win.cpp
@@ -34,9 +34,9 @@ Win::Win(QWidget *parent):QWidget(parent)
     hLayout->addLayout(vLayout2);
 
     begin();
-    connect(exitButton,SIGNAL(clicked(bool)), this, SLOT(close()));
-    connect(nextButton,SIGNAL(clicked(bool)), this, SLOT(begin()));
-    connect(inputEdit,SIGNAL(returnPressed()), this, SLOT(calc()));
+    connect(exitButton,&QPushButton::clicked, this, &Win::close);
+    connect(nextButton,&QPushButton::clicked, this, &Win::begin);
+    connect(inputEdit,&QLineEdit::returnPressed, this, &Win::calc);
 }
 void Win::begin()
 {
