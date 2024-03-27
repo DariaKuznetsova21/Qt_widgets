2d46df7f80f3e8fcc27a67c0e37401e7d60fb19d
Daria <dariakuznetsova21@gmail.com>
2024-03-27 11:10:17 +0700

    Add lab 2.7

--- win.h
+++ win.h
@@ -1,22 +1,34 @@
 #ifndef WIN_H
 #define WIN_H
+#include <QtWidgets>
 
-#include <QMainWindow>
-
-namespace Ui {
-class Win;
-}
-
-class Win : public QMainWindow
+class Win: public QWidget // класс окна
 {
-    Q_OBJECT
-
+    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+protected:
+//    QTextCodec *codec;
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
 public:
-    explicit Win(QWidget *parent = 0);
-    ~Win();
+    Win(QWidget *parent = 0); // конструктор
+public slots:
+    void begin(); // метод начальной настройки интерфейса
+    void calc(); // метод реализации вычислений
+};
 
-private:
-    Ui::Win *ui;
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    {
+        return Acceptable; // метод всегда возвращат вводимую строку
+    }
 };
 
 #endif // WIN_H
