6aa6fdff62ada9ba2b9d0deac6b0fb829e1ef081
Daria <dariakuznetsova21@gmail.com>
2024-03-27 11:14:02 +0700

    Add lab 2.8

--- win.h
+++ win.h
@@ -1,22 +1,39 @@
 #ifndef WIN_H
 #define WIN_H
+#include <QtWidgets>
 
-#include <QMainWindow>
-
-namespace Ui {
-class Win;
-}
-
-class Win : public QMainWindow
+class Counter:public QLineEdit
 {
-    Q_OBJECT
-
+     Q_OBJECT
 public:
-    explicit Win(QWidget *parent = 0);
-    ~Win();
-
-private:
-    Ui::Win *ui;
+    Counter(const QString & contents, QWidget *parent=0):
+        QLineEdit(contents,parent){}
+signals:
+    void tick_signal();
+public slots:
+    void add_one()
+    {
+        QString str=text();
+        int r=str.toInt();
+        if (r!=0 && r%5 ==0) emit tick_signal();
+        r++;
+        str.setNum(r);
+        setText(str);
+    }
 };
 
-#endif // WIN_H
+class Win: public QWidget
+{
+    Q_OBJECT
+protected:
+    //QTextCodec *codec;
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcbutton;
+    QPushButton *exitbutton;
+public:
+    Win(QWidget *parent = 0);
+};
+
+#endif
+
